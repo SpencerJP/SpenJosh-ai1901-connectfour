6e687c39a91dec4563e21f18dadd58dddb75cd65
Splerge <damnsplurge@gmail.com>
2019-04-18 20:00:32 +1000

    made changes from testing branch

--- connectfour/agents/agent_student.py
+++ connectfour/agents/agent_student.py
@@ -1,6 +1,7 @@
 """Spencer and Josh's agent for playing Connect 4."""
-import math
 import time
+import copy
+from connectfour.board import Board
 from connectfour.agents.computer_player import RandomAgent
 
 
@@ -13,14 +14,17 @@ def get_current_player(num_moves):
         return PLAYER_ONE_ID
     return PLAYER_TWO_ID
 
-def debug_print_board(board):
+def debug_print_board(board, score=None):
     """Prints out the board argument into the terminal, followed by a newline."""
     string = ""
     for row in board.board:
         for cell in row:
             string += str(cell)
         string += str("\n")
+    if score:
+        string += str("\nScore: " + str(score))
     print(string)
+
 
 def count_moves(board):
     """counts the amount of tokens that have been inserted into the board."""
@@ -33,9 +37,25 @@ def count_moves(board):
     return sum_of_moves
 
 
+
+def convertStringToBoard(s, boardclass):
+    row = 0
+    col = 0
+    string = ""
+    for character in s:
+        if(col == 7):
+            row = row + 1
+            col = 0
+            boardclass.board[row][col] = int(character)
+            col = col + 1
+        else:
+            boardclass.board[row][col] = int(character)
+            col = col + 1
+
+
 def valid_moves_wrapper(board):
     """Wrap the board.valid_moves() generator in our own organiser that
-    orders the moves centre to outside, going left first if it is uneven."""
+    orders the moves centre to outside, going right first if it is uneven."""
 
     #this optimisation is specific to 6*7 boards, if it isn't 7 wide then abort.
     if board.width != 7:
@@ -67,7 +87,7 @@ def valid_non_losing_moves(board, num_moves):
     valid_moves = valid_moves_wrapper(board)
     #loop through each move
     for move in valid_moves:
-        my_move = board.next_state(current_player, move[1])
+        my_move = next_state_fast(board, current_player, move)
         winner_num = my_move.winner()
         #if we win then this is a valid move that won't cause us to lose,
         #and there is no reason to continue the search either.
@@ -80,7 +100,7 @@ def valid_non_losing_moves(board, num_moves):
         #if there is a single move that they can make in which they can win,
         #this whole move of ours is a bust, so don't yield it.
         for enemy_move in enemy_valid_moves:
-            node_after = my_move.next_state(other_player, enemy_move[1])
+            node_after = next_state_fast(my_move, other_player, enemy_move)
             winner_num = node_after.winner()
             if winner_num != 0:
                 failure = True
@@ -104,20 +124,20 @@ def count_non_losing_moves(board, num_moves):
     sum_of_moves = 0
     #loop through each move
     for move in valid_moves:
-        my_move = board.next_state(current_player, move[1])
+        my_move = next_state_fast(board, current_player, move)
         winner_num = my_move.winner()
         #if we win then this is a valid move that won't cause us to lose,
         #and there is no reason to continue the search either,
-        #we can safely return one so that the algorithm continues.
+        #we can safely return -one so that the algorithm understands that it can win.
         if winner_num != 0:
-            return 1
+            return -1
         enemy_valid_moves = my_move.valid_moves()
         failure = False
         #loop through the enemy's moves after our move.
         #if there is a single move that they can make in which they can win,
         #this whole move of ours is a bust, so don't yield it.
         for enemy_move in enemy_valid_moves:
-            node_after = my_move.next_state(other_player, enemy_move[1])
+            node_after = next_state_fast(my_move, other_player, enemy_move)
             winner_num = node_after.winner()
             if winner_num != 0:
                 failure = True
@@ -126,6 +146,19 @@ def count_non_losing_moves(board, num_moves):
         if not failure:
             sum_of_moves += 1
     return sum_of_moves
+
+
+def next_state_fast(board, player_id, move):
+    """My monkey patching method to avoid using deepcopy"""
+    next_board = Board(width=7, height=6, last_move=move)
+    next_board.board = [x[:] for x in [[0] * 7] * 6] # superfast way to declare 2d array
+    for i in range(board.height):
+        for j in range(board.width):
+            next_board.board[i][j] = board.board[i][j]
+    next_board.board[move[0]][move[1]] = player_id
+    next_board.next_state_fast = next_state_fast
+    next_board.winning_zones = board.winning_zones
+    return next_board
 
  # pylint: disable=too-many-instance-attributes
 class StudentAgent(RandomAgent):
@@ -136,7 +169,7 @@ class StudentAgent(RandomAgent):
         self.id = -1
         self.dimensions = -1
         self.enemy_id = -1
-        self.debug = True
+        self.debug = False
         self.transpos_table = {}
         self.middle_col = -1
         self.max_score = -1
@@ -152,6 +185,7 @@ class StudentAgent(RandomAgent):
         """
         start = time.time()
 
+        board.next_state_fast = next_state_fast
         #check how many moves have occurred so far on this board.
         current_move_number = count_moves(board)
 
@@ -171,12 +205,10 @@ class StudentAgent(RandomAgent):
             self.max_depth = 1
         elif current_move_number < 8:
             self.max_depth = 3
-        elif current_move_number < 12:
+        elif current_move_number < 18:
             self.max_depth = 4
-        elif current_move_number < 15:
-            self.max_depth = 5
-        elif current_move_number < 23:
-            self.max_depth = 8
+        elif current_move_number < 25:
+            self.max_depth = 7
         elif current_move_number > 30:
             self.max_depth = 10
             # self.max_depth = min([int(math.sqrt(current_move_number)) + 1, 5])
@@ -191,11 +223,21 @@ class StudentAgent(RandomAgent):
         #we lose, concede
         if non_losing_moves_count == 0:
             valid_moves = valid_moves_wrapper(board)
-            return next(valid_moves)
+            best_move = next(valid_moves)
+            if self.debug:
+                print("Placed a piece in (%d, %d)" % (best_move[0], best_move[1]))
+                next_node = next_state_fast(board, self.id, best_move)
+                debug_print_board(next_node)
+            return best_move
         #we only have one available move
         if non_losing_moves_count == 1:
             valid_moves = valid_non_losing_moves(board, current_move_number)
-            return next(valid_moves)
+            best_move = next(valid_moves)
+            if self.debug:
+                print("Placed a piece in (%d, %d)" % (best_move[0], best_move[1]))
+                next_node = next_state_fast(board, self.id, best_move)
+                debug_print_board(next_node)
+            return best_move
         if non_losing_moves_count == 7:
             #get all moves
             valid_moves = valid_moves_wrapper(board)
@@ -211,10 +253,10 @@ class StudentAgent(RandomAgent):
             minimum = int(-(self.dimensions - current_move_number) / 2)
 
             maximum = int((self.dimensions + 1 - current_move_number) / 2)
-            next_node = board.next_state(self.id, move[1])
+            next_node = next_state_fast(board, self.id, move)
             moves.append(move)
 
-            value = int(self.negamax(next_node, minimum, maximum, current_move_number))
+            value = int(self.minimax_alpha_beta(next_node, minimum, maximum, current_move_number))
             if self.debug:
                 print("column number: %d, calculated value: %d" % (move[1], value))
             vals.append(value)
@@ -228,7 +270,7 @@ class StudentAgent(RandomAgent):
             valid_moves = board.valid_moves()
             best_move = next(valid_moves)
 
-        next_node = board.next_state(self.id, best_move[1])
+        next_node = next_state_fast(board, self.id, best_move)
 
         end = time.time()
         if self.debug:
@@ -241,7 +283,7 @@ class StudentAgent(RandomAgent):
         return best_move
 
 
-    def negamax(self, board, alpha, beta, num_moves, sign=1, depth=0):
+    def minimax_alpha_beta(self, board, alpha, beta, num_moves, sign=1, depth=0):
         """returns score of the board position
 
         board is the game state to evaluate.
@@ -260,78 +302,78 @@ class StudentAgent(RandomAgent):
         depth is how deep our search has gone so far, beginning at 0 from get_move().
         """
 
+
+
+
+        # no valid moves that won't cause a loss, aka dead end
+
+        sum_of_moves = count_non_losing_moves(board, num_moves)
+        if sum_of_moves == 0:
+            return sign * -int((self.dimensions - num_moves) / 2)
+
         #check if this board has a winner and return if it does
         #this is the heuristic of our algorithm.
         #The number it returns is scored on how many moves
         #it would take to guarantee a victory for a perfect player.
         winner_num = board.winner()
         if winner_num != 0:
-            if winner_num == self.id:
-                return  sign * int((self.dimensions - num_moves) / 2)
-            return sign * -int((self.dimensions - num_moves) / 2)
+            return sign * int((self.dimensions - num_moves) / 2)
 
-        # no valid moves that won't cause a loss, aka dead end
-        sum_of_moves = count_non_losing_moves(board, num_moves)
-        if sum_of_moves == 0:
-            return sign * -(self.dimensions - num_moves) / 2
+        if depth == self.max_depth:
+            return sign * self.evaluate_board_state(board, num_moves)
 
         #detect a draw, once 40 tokens are on the board in a 6*7 game and no one has won already,
         #no one can possibly win now.
         if num_moves >= self.dimensions - 2:
             return 0
 
-        # set alpha to the minimum possible value
-        minimum = int(-(self.dimensions - 2 - num_moves) / 2)
-        if alpha < minimum:
-            alpha = minimum
-            if alpha >= beta:
-                return alpha #prune children.
-
-        # set beta to the maximum possible value
-        maximum = int((self.dimensions - 1 - num_moves) / 2)
-        if beta > maximum:
-            beta = maximum
-            if alpha >= beta:
-                return beta  #prune children.
-
-        trans_value = self.transpos_table.get(str(board.board))
-        if trans_value:
-            if trans_value <= alpha:
-                return alpha
-            if trans_value >= beta:
-                return beta
-            return trans_value
-
-        if depth == self.max_depth:
-            max_depthvalue = self.evaluate_board_state(board, num_moves)
-            if max_depthvalue <= alpha:
-                return alpha
-            if max_depthvalue >= beta:
-                return beta
-            return max_depthvalue
+        # # set alpha to the minimum possible value
+        # minimum = int(-(self.dimensions - 2 - num_moves) / 2)
+        # if alpha < minimum:
+        #     alpha = minimum
+        #     if alpha >= beta:
+        #         return alpha #prune children.
+        #
+        # # set beta to the maximum possible value
+        # maximum = int((self.dimensions - 1 - num_moves) / 2)
+        # if beta > maximum:
+        #     beta = maximum
+        #     if alpha >= beta:
+        #         return beta  #prune children.
 
 
-        valid_moves = valid_non_losing_moves(board, num_moves)
+        valid_moves = valid_moves_wrapper(board)
+        vals = []
+        if sign == 1:
+            value = 1000
+            for move in valid_moves:
+                next_node = next_state_fast(board, get_current_player(num_moves+1), move)
+                # recursively go through the children of this node.
+                result = self.minimax_alpha_beta(next_node, alpha, beta, num_moves+1, -sign, depth + 1)
+                vals.append(result)
 
+                if result < value:
+                    value = result
+                if value < beta:
+                    beta = value
+
+                if alpha >= beta:
+                    break
+            return value
+        value = -1000
         for move in valid_moves:
-            next_node = board.next_state(get_current_player(num_moves+1), move[1])
+            next_node = next_state_fast(board, get_current_player(num_moves+1), move)
             # recursively go through the children of this node.
-            result = -self.negamax(next_node, -beta, -alpha, num_moves+1, -sign, depth + 1)
+            result = self.minimax_alpha_beta(next_node, alpha, beta, num_moves+1, -sign, depth + 1)
+            vals.append(result)
+            if result > value:
+                value = result
+            if value > alpha:
+                alpha = value
 
-
-
-            if result >= beta:
-                return result
-
-            # if the result is bigger than the current minimum, set the minimum to the new result
-            if result > alpha:
-                alpha = result
-
-        return alpha
-
-
-
-
+            if alpha >= beta:
+                break
+        return value
 
     def evaluate_board_state(self, board, num_moves):
 
